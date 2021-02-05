ef9dc0403c06de76531df7ff3eb2b5dfbfd6a901
Kira Mironova <kiracabezas@gmail.com>
2021-02-05 10:26:16 +0100

    update

--- InterfazGrafica/src/main/java/com/mycompany/interfazgrafica/Redimensionable.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.interfazgrafica;
-
-public interface Redimensionable {
-    public void setAltura(int pixelsAltura);
-    public void setAnchura(int pixelsAnchura);
-    public void setDimension(int pixelsAltura, int pixelsAnchura);
-}
